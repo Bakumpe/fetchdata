c166b1712700cee00b4d02d5efb5e566e72243f9
Bakumpe <bakumpejoseph@gmail.com>
2025-01-29 03:18:46 +0300

    Fetch Data

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-function LandingPage(){
-    return(
-        <>
-        <h1>Landing Page</h1>
-        </>
-    )
+function LandingPage() {
+  return (
+    <>
+      <div className="bodyPost">
+        <div className="postBody">
+          <h1>Landing Page</h1>
+          <p>Please Navigate to (Posts Page) to view the Posts</p>
+          <p>Thanks for the Visit</p>
+        </div>
+      </div>
+    </>
+  );
 }
-export default LandingPage
+export default LandingPage;
